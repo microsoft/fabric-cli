661f13d8d874b8e8e5883400ce4f3a46257bc154
CSharplie <charlie.collier@outlook.fr>
2025-11-05 19:11:40 +0000

    Print warning to delete and in progress message

--- src/fabric_cli/commands/jobs/fab_jobs_run_rm.py
+++ src/fabric_cli/commands/jobs/fab_jobs_run_rm.py
@@ -12,7 +12,12 @@ from fabric_cli.utils import fab_ui as utils_ui
 
 
 def exec_command(args: Namespace, context: Item) -> None:
+    if not args.force:
+        utils_ui.print_warning(f"You are about to delete schedule '{args.schedule_id}' from '{context.full_name}' This action cannot be undone.")
+
     if args.force or utils_ui.prompt_confirm():
+        utils_ui.print_grey(f"Removing job schedule '{args.schedule_id}'... from '{context.full_name}'")
+
         response = jobs_api.remove_item_schedule(args)
 
         if response.status_code == 200:
