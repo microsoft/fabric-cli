64e65fd08e1bba38ba3fb230d71933f2120f4c8a
CSharplie <charlie.collier@outlook.fr>
2025-11-05 09:04:22 +0000

    use utils_ui alias

--- src/fabric_cli/commands/jobs/fab_jobs_run_rm.py
+++ src/fabric_cli/commands/jobs/fab_jobs_run_rm.py
@@ -8,15 +8,15 @@ from fabric_cli.client import fab_api_jobs as jobs_api
 from fabric_cli.core import fab_constant
 from fabric_cli.core.fab_exceptions import FabricCLIError
 from fabric_cli.core.hiearchy.fab_hiearchy import Item
-from fabric_cli.utils import fab_ui
+from fabric_cli.utils import fab_ui as utils_ui
 
 
 def exec_command(args: Namespace, context: Item) -> None:
-    if args.force or fab_ui.prompt_confirm():
+    if args.force or utils_ui.prompt_confirm():
         response = jobs_api.remove_item_schedule(args)
 
         if response.status_code == 200:
-            fab_ui.print_output_format(
+            utils_ui.print_output_format(
                 args,
                 message=f"Job schedule {args.schedule_id} removed",
             )
