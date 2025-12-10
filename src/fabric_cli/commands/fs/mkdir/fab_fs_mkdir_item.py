fc07b21c9a4bde335a079f75c530141c66172cb6
aviatco <32952699+aviatco@users.noreply.github.com>
2025-12-10 09:03:33 +0200

    Update src/fabric_cli/commands/fs/mkdir/fab_fs_mkdir_item.py
    Co-authored-by: Alon Yeshurun <98805507+ayeshurun@users.noreply.github.com>

--- src/fabric_cli/commands/fs/mkdir/fab_fs_mkdir_item.py
+++ src/fabric_cli/commands/fs/mkdir/fab_fs_mkdir_item.py
@@ -63,7 +63,7 @@ def exec(item: Item, args: Namespace) -> str | None:
     if response.status_code in (200, 201):
         data = json.loads(response.text)
         
-        if hasattr(args, 'output_batch') and args.output_batch['is_batching']:
+        if not is_root_operation and args.output_batch['is_batching'] == True:
             # Collect operation data for batch output
             args.output_batch['items'].append(data)
             args.output_batch['names'].append(item.name)
