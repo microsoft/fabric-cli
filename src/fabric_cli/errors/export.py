872b5a17fa56f454b04c9092996d516c5251e5bd
Jerome Kafrouni <j.kafrouni@gmail.com>
2025-12-16 09:29:42 -0500

    Remove trailing dot in export error message

--- src/fabric_cli/errors/export.py
+++ src/fabric_cli/errors/export.py
@@ -5,4 +5,6 @@
 class ExportErrors:
     @staticmethod
     def invalid_export_format(valid_formats: list[str]) -> str:
-        return f"Invalid format. Only the following formats are supported: {valid_formats}."
+        return (
+            f"Invalid format. Only the following formats are supported: {valid_formats}"
+        )
