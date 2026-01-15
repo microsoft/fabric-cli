bd72b7b29fa26f458e29c4f6ef0a05eee075553b
Alon Yeshurun <alonyeshurun+microsoft@microsoft.com>
2026-01-15 10:31:54 +0000

    Fix dev container installation

--- scripts/install_dev_container_dependencies.sh
+++ scripts/install_dev_container_dependencies.sh
@@ -1,9 +1,13 @@
+#!/usr/bin/env bash
+set -e
+
 apt-get update && apt-get install -y \
     cmake \
     libcairo2-dev \
     pkg-config \
-    python3-dev \
+    python3-dev
 
-pip3 install --user -e .[dev]
+pip3 install --user -e .
+pip3 install --user -r requirements-dev.txt -r requirements-docs.txt
 
 npm install -g changie
