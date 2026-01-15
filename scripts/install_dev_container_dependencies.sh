88019533d1ee041a32bcc809a32ea70197adf005
Alon Yeshurun <alonyeshurun@microsoft.com>
2026-01-15 13:18:20 +0000

    Small update

--- scripts/install_dev_container_dependencies.sh
+++ scripts/install_dev_container_dependencies.sh
@@ -7,7 +7,7 @@ apt-get update && apt-get install -y \
     pkg-config \
     python3-dev
 
-pip3 install --user -e .
-pip3 install --user -r requirements-dev.txt -r requirements-docs.txt
+sudo pip3 install -e .
+pip3 install -r requirements-dev.txt -r requirements-docs.txt
 
 npm install -g changie
