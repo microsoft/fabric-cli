8d551675e67bdb07f6c85fe02d9886287998314a
Aviat Cohen <aviatcohen@microsoft.com>
2026-03-05 14:06:30 +0000

    resolve backslash issue in prompt message

--- src/fabric_cli/commands/fs/fab_fs_deploy.py
+++ src/fabric_cli/commands/fs/fab_fs_deploy.py
@@ -9,6 +9,6 @@ from fabric_cli.utils import fab_ui
 
 def exec_command(args: Namespace) -> None:
     """deploy fabric items to a workspace using a configuration file and target environment - CICD flow."""
-    prompt = f"Are you sure you want to deploy {'without a target environment' if args.target_env == 'N/A' else f'to target environment \'{args.target_env}\''} using the specified configuration file?"
-    if args.force or fab_ui.prompt_confirm(prompt):
+    target_env_msg = 'without a target environment' if args.target_env == 'N/A' else f"to target environment '{args.target_env}'"
+    if args.force or fab_ui.prompt_confirm(f"Are you sure you want to deploy {target_env_msg} using the specified configuration file?"):
         deploy_with_config_file(args)
